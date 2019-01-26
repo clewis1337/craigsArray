c16a3ef6be397072e76a12570d2bcb1b41870bcf
CLewis <clewis1337@gmail.com>
2019-01-25 18:23:10 -0800

    Testing

--- craigs-array/src/App.js
+++ craigs-array/src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    
+  };
+
+  function = id => {
+    
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Header />
+        
+      </Wrapper>
     );
   }
 }
---
+++ craigs-array/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header class="header navbar-fixed-top">Testing</header>
+    
+    );
+  }
+}
+
+export default Header;
